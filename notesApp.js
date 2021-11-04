67c4f2b98c363ec634681122a9d28bc9922ad9e8
Aldin <alhuric89@gmail.com>
2021-11-04 17:33:16 +0100

    add notes obj

--- notesApp.js
+++ notesApp.js
@@ -2,7 +2,18 @@
 // query and remove
 /*const para = document.querySelector("p");
 para.remove();*/
-
+const notes = [{
+    title: "my next trip",
+    body: "I would like to go to Spain"
+},
+{
+    title: "Habbits to work on",
+    body: "Exercise. Eating a bit better"
+},
+{
+    title: "office modification",
+    body: "Get a new seat"
+},]
 
 const ps = document.querySelectorAll("p")
 
@@ -11,3 +22,9 @@ ps.forEach((e)=> {
     //console.log(e.textContent);
 })
 
+
+const newParagraph = document.createElement("p");
+newParagraph.textContent = "This is new element from JS";
+
+
+document.body.appendChild(newParagraph); 

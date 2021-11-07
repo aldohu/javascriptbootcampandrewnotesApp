aaa8f28d204bccf442dafc47f3638c4c1ee5c213
Aldin <alhuric89@gmail.com>
2021-11-07 20:21:59 +0100

    add new feature to app

--- notes-app.js
+++ notes-app.js
@@ -1,18 +1,20 @@
-const notes = [{
-    title: 'my next trip',
-    body: 'I would like to go to Spain'
-}, {
-    title: 'Habbits to work on',
-    body: 'Exercise. Eating a bit better.'
-}, {
-    title: 'Office modification',
-    body: 'Get a new seat'
-}]
+let notes = []
 
 const filters = {
     searchText: ''
 }
+// check for existing saved data
+const notesJSON = localStorage.getItem("notes");
 
+if(notesJSON !== null){
+    notes = JSON.parse(notesJSON);
+}
+
+
+const user = {
+    name: "Andrew",
+    age: 27
+}
 const renderNotes = function (notes, filters) {
     const filteredNotes = notes.filter(function (note) {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
@@ -22,24 +24,29 @@ const renderNotes = function (notes, filters) {
     
     filteredNotes.forEach(function (note) {
         const noteEl = document.createElement('p')
-        noteEl.textContent = note.title
-        document.querySelector('#notes').appendChild(noteEl)
-    })
+        if(note.title.length> 0) {
+        noteEl.textContent = note.title;   
+    }else {
+        noteEl.textContent= "Unnamed note"
+    }
+    document.querySelector('#notes').appendChild(noteEl);
+})
 }
-
 renderNotes(notes, filters)
-
 document.querySelector('#create-note').addEventListener('click', function (e) {
-    e.target.textContent = 'The button was clicked'
+   notes.push({
+       title:"",
+       body: ""
+   });
+   localStorage.setItem("notes", JSON.stringify(notes));
+   renderNotes(notes,filters)
 })
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
-    filters.searchText = e.target.value
-    renderNotes(notes, filters)
+    filters.searchText = e.target.value;
+    renderNotes(notes, filters);
 })
 
-document.querySelector('#name-form').addEventListener('submit', function (e) {
-    e.preventDefault()
-    console.log(e.target.elements.firstName.value)
-    e.target.elements.firstName.value = ''
-})
+document.querySelector("#filter-by").addEventListener("change",function(e){
+    console.log(e.target.value);
+});

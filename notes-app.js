0ef196b22bf5cde32c4b974e3752f4d7235e02a3
Aldin <alhuric89@gmail.com>
2021-11-07 20:36:12 +0100

    add refactoring

--- notes-app.js
+++ notes-app.js
@@ -1,20 +1,10 @@
-let notes = []
+const notes = getSavedNotes();
 
 const filters = {
     searchText: ''
 }
 // check for existing saved data
-const notesJSON = localStorage.getItem("notes");
 
-if(notesJSON !== null){
-    notes = JSON.parse(notesJSON);
-}
-
-
-const user = {
-    name: "Andrew",
-    age: 27
-}
 const renderNotes = function (notes, filters) {
     const filteredNotes = notes.filter(function (note) {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
@@ -23,12 +13,7 @@ const renderNotes = function (notes, filters) {
     document.querySelector('#notes').innerHTML = ''
     
     filteredNotes.forEach(function (note) {
-        const noteEl = document.createElement('p')
-        if(note.title.length> 0) {
-        noteEl.textContent = note.title;   
-    }else {
-        noteEl.textContent= "Unnamed note"
-    }
+       const noteEl = generateNoteDOM(note);
     document.querySelector('#notes').appendChild(noteEl);
 })
 }

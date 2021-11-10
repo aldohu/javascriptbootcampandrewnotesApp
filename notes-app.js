4c67b4ec921921cf93c14921871e49253f948ac2
Aldin <alhuric89@gmail.com>
2021-11-10 17:39:19 +0100

    add edit html file

--- notes-app.js
+++ notes-app.js
@@ -1,27 +1,26 @@
-
+const notes = getSavedNotes()
 
 const filters = {
     searchText: ''
 }
-// check for existing saved data
-const notes = getSavedNotes();
-
 
 renderNotes(notes, filters)
+
 document.querySelector('#create-note').addEventListener('click', function (e) {
-   notes.push({
-       title:"",
-       body: ""
-   });
-   localStorage.setItem("notes", JSON.stringify(notes));
-   renderNotes(notes,filters)
+    notes.push({
+        id: uuidv4(),
+        title: '',
+        body: ''
+    })
+    saveNotes(notes)
+    renderNotes(notes, filters)
 })
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
-    filters.searchText = e.target.value;
-    renderNotes(notes, filters);
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
 })
 
-document.querySelector("#filter-by").addEventListener("change",function(e){
-    console.log(e.target.value);
-});
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+    console.log(e.target.value)
+})
